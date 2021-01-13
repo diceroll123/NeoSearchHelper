c589318bb1be958f11da9c76185c972d6ecc6fb0
Steve C <diceroll123@gmail.com>
2021-01-13 15:12:01 -0800

    Version bump 1.0.7

--- searchhelper.user.js
+++ searchhelper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name       Neopets - Search Helper
-// @version    1.0.6
+// @version    1.0.7
 // @match      http://www.neopets.com/halloween/witchtower*.phtml
 // @match      http://www.neopets.com/island/kitchen*.phtml
 // @match      http://www.neopets.com/medieval/earthfaerie.phtml*
