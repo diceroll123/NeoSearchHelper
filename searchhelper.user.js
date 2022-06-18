7395fc0db355ee144d6cf1dbe553ffe4c4ef082d
Steve C <diceroll123@gmail.com>
2022-06-18 16:54:13 -0400

    Version bump 1.0.16

--- searchhelper.user.js
+++ searchhelper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name       Neopets - Search Helper
-// @version    1.0.15
+// @version    1.0.16
 // @match      *://www.neopets.com/auctions.phtml*
 // @match      *://www.neopets.com/closet.phtml*
 // @match      *://www.neopets.com/dome/neopets.phtml*
