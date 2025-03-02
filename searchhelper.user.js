b44ce8be3db547bd299b1e9aba2a23fe1c668750
Steve C <diceroll123@gmail.com>
2025-03-02 12:11:08 -0500

    Version bump 1.0.28

--- searchhelper.user.js
+++ searchhelper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Neopets - Search Helper
-// @version      1.0.27
+// @version      1.0.28
 // @namespace    neopets
 // @description  Adds convenient search icons to many places
 // @author       diceroll123
